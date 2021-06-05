38fd3e4e89ca29af4e8c1187f25287c1e93aadbc
Vany_H <johnny.work.univer@gmail.com>
2021-06-05 11:57:40 +0300

    enemy Move

--- js/autoMove.js
+++ js/autoMove.js
@@ -1,44 +1,32 @@
 let characterEnemy = {
-    obj:document.querySelector(".enemyTank"),
-    position:findStart(fieldArray, height, width),
-    rotation: Math.PI
+  obj: document.querySelector(".enemyTank"),
+  position: findStart(fieldArray, height, width),
+  rotation: Math.PI,
 };
 
-characterEnemy.obj.style.top = (characterEnemy.position.y * sizeBlock)+"px";
-characterEnemy.obj.style.left = (characterEnemy.position.x * sizeBlock)+"px";
-characterEnemy.obj.style.rotate = ((360*characterEnemy.rotation) / (Math.PI*2))+"deg";
+let way = [];
 
-function enemyMove(event, character){
+characterEnemy.obj.style.top = characterEnemy.position.y * sizeBlock + "px";
+characterEnemy.obj.style.left = characterEnemy.position.x * sizeBlock + "px";
+characterEnemy.obj.style.rotate =
+  (360 * characterEnemy.rotation) / (Math.PI * 2) + "deg";
 
-    switch (event.key){
-        case "w" || "W":
-            let checkPoint = checkWay(fieldArray, character);
+function autoMove() {
+  let checkChar = userWayEn(fieldArray, characterEnemy);
 
-            if(checkPoint !== null){
-                character.position = checkPoint;
-            }
-        break;
+  if (checkChar !== null && checkChar !== 0) {
+    // console.log(characterEnemy);
+    characterEnemy = checkChar;
+  } else {
+    characterEnemy.rotation += Math.PI / 2;
+  }
 
-        case "a" || "A":
-            character.rotation += ((Math.PI/2));
-        break;
+  console.log(characterEnemy.obj.style.rotate);
 
-        case "d" || "D":
-            character.rotation -= ((Math.PI/2));
-        break;
-    }
-
-    character.obj.style.top = (character.position.y * sizeBlock)+"px";
-    character.obj.style.left = (character.position.x * sizeBlock)+"px";
-    character.obj.style.transform = "rotate("+((360*character.rotation) / (Math.PI*2))+"deg)";
+  characterEnemy.obj.style.top = characterEnemy.position.y * sizeBlock + "px";
+  characterEnemy.obj.style.left = characterEnemy.position.x * sizeBlock + "px";
+  characterEnemy.obj.style.transform =
+    "rotate(" + (360 * characterEnemy.rotation) / (Math.PI * 2) + "deg)";
 }
 
-function autoMove(){
-    let point;
-    
-    characterEnemy.position = point;
-    characterEnemy.obj.style.top = (characterEnemy.position.y * sizeBlock)+"px";
-    characterEnemy.obj.style.left = (characterEnemy.position.x * sizeBlock)+"px";
-}
-
-setInterval(autoMove, 500);
+var timer = setInterval(autoMove, 500);
--- js/funct.js
+++ js/funct.js
@@ -1,160 +1,227 @@
-function checkVisit(height, width, array){
-    let count = 0;
+function checkVisit(height, width, array) {
+  let count = 0;
 
-    for(let i=0; i<height; i++){
-        for(let j=0; j<width; j++){
-            if(array[i][j] === 1){
-                count ++
-            }
-        }
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (array[i][j] === 1) {
+        count++;
+      }
     }
+  }
 
-    return count;
+  return count;
 }
 
-function checkNeighbours(array, coord, step){
-    const allNeighbours = [
-        {x: coord.x, y: coord.y+step}, 
-        {x: coord.x, y: coord.y-step}, 
-        {x: coord.x+step, y: coord.y}, 
-        {x: coord.x-step, y: coord.y}
-    ];
-    let neighbours = [];
+function checkNeighbours(array, coord, step) {
+  const allNeighbours = [
+    { x: coord.x, y: coord.y + step },
+    { x: coord.x, y: coord.y - step },
+    { x: coord.x + step, y: coord.y },
+    { x: coord.x - step, y: coord.y },
+  ];
+  let neighbours = [];
 
-    for(let i=0; i<4; i++){
-        if(allNeighbours[i].x > 0 && allNeighbours[i].x < width && allNeighbours[i].y > 0 && allNeighbours[i].y < height){
-            if(array[allNeighbours[i].y][allNeighbours[i].x] !== 2){
-                neighbours.push(allNeighbours[i]);
-            }
-        }
+  for (let i = 0; i < 4; i++) {
+    if (
+      allNeighbours[i].x > 0 &&
+      allNeighbours[i].x < width &&
+      allNeighbours[i].y > 0 &&
+      allNeighbours[i].y < height
+    ) {
+      if (array[allNeighbours[i].y][allNeighbours[i].x] !== 2) {
+        neighbours.push(allNeighbours[i]);
+      }
     }
+  }
 
-    if(neighbours.length != 0){
-        return neighbours[Math.floor(Math.random()*neighbours.length)];
-    }else{
-        return null;
-    }
-}
-
-function checkNeighboursEn(array, coord, step){
-    const allNeighbours = [
-        {x: coord.x, y: coord.y+step}, 
-        {x: coord.x, y: coord.y-step}, 
-        {x: coord.x+step, y: coord.y}, 
-        {x: coord.x-step, y: coord.y}
-    ];
-    let neighbours = [];
-
-    for(let i=0; i<4; i++){
-        if(allNeighbours[i].x > 0 && allNeighbours[i].x < width && allNeighbours[i].y > 0 && allNeighbours[i].y < height){
-            if(array[allNeighbours[i].y][allNeighbours[i].x] === 2){
-                neighbours.push(allNeighbours[i]);
-            }
-        }
-    }
-
-    console.log(neighbours);
-
-    if(neighbours.length != 0){
-        return neighbours[Math.floor(Math.random()*neighbours.length)];
-    }else{
-        return null;
-    }
-}
-
-function newMaze(array, coord1, coord2){
-    let newArray = array; 
-    let xBuff = coord2.x - coord1.x;
-    let yBuff = coord2.y - coord1.y;
-
-    addX = (xBuff != 0) ? (xBuff/Math.abs(xBuff)) : 0;
-    addY = (yBuff != 0) ? (yBuff/Math.abs(yBuff)) : 0;
-
-    newArray[coord1.y][coord1.x] = 2;
-    newArray[coord2.y][coord2.x] = 2;
-    newArray[coord1.y + addY][coord1.x + addX] = 2;
-
-    return newArray;
-}
-
-function addStartEnd(array, height, width){
-    let newArray = array;
-    let startEnd = Math.floor(Math.random() * width);
-    
-    while(array[1][startEnd] !== 2){
-        startEnd = Math.floor(Math.random() * width);
-    }
-    newArray[0][startEnd] = 3
-
-    startEnd = Math.floor(Math.random() * width);
-    while(array[height-2][startEnd] !== 2){
-        startEnd = Math.floor(Math.random() * width);
-    }
-    newArray[height-1][startEnd] = 4;
-
-    return newArray;
-}
-
-function findStart(array, height, width){
-    let point = {
-        x:0,
-        y:0
-    };
-
-    for(let i=0; i<height; i++){
-        for(let j=0; j<width; j++){
-            if(array[i][j] === 3){
-                point.x = j;
-                point.y = i;
-
-                return point;
-            }
-        }
-    }
-}
-
-function findEnd(array, height, width){
-    let point= {
-        x:0,
-        y:0
-    };
-
-    for(let i=0; i<height; i++){
-        for(let j=0; j<width; j++){
-            if(array[i][j] === 4){
-                point.x = j;
-                point.y = i;
-
-                return point;
-            }
-        }
-    }
-}
-
-function checkWay(array, character){
-    let positionBuff = character.position;
-       
-    let X = positionBuff.x + Math.round(1*Math.sin(character.rotation));
-    let Y = positionBuff.y + Math.round(1*Math.cos(character.rotation) * -1);
-
-    if(positionBuff.y >= 0 && positionBuff.y < array.length && positionBuff.x >= 0 && positionBuff.x < array[0].length){
-        if(array[Y][X] === 2){
-            positionBuff.x = X;
-            positionBuff.y = Y;
-            return positionBuff;
-        } else if(array[Y][X] === 4){
-            let masField = document.querySelectorAll(".field>div");
-
-            for(let i = 0; i<masField.length; i++){
-                masField[i].remove();
-            }
-
-            height += 4;
-            width += 4;
-            Draw();
-
-            character.position = findStart(fieldArray, height, width);
-        }
-    }
+  if (neighbours.length != 0) {
+    return neighbours[Math.floor(Math.random() * neighbours.length)];
+  } else {
     return null;
+  }
+}
+
+function checkWayEn(array, character) {
+  const positionBuff = character.position;
+  let rotateBuff = character.rotation + Math.PI / 2;
+  let characterBuff = character;
+
+  let X = positionBuff.x + Math.round(1 * Math.sin(rotateBuff));
+  let Y = positionBuff.y + Math.round(1 * Math.cos(rotateBuff) * -1);
+
+  if (
+    positionBuff.y >= 0 &&
+    positionBuff.y < array.length &&
+    positionBuff.x >= 0 &&
+    positionBuff.x < array[0].length
+  ) {
+    if (array[Y][X] === 4) {
+      clearInterval(timer);
+      return 0;
+    } else {
+      X = positionBuff.x + Math.round(1 * Math.sin(character.rotation));
+      Y = positionBuff.y + Math.round(1 * Math.cos(character.rotation) * -1);
+
+      if (array[Y][X] === 4) {
+        clearInterval(timer);
+        return 0;
+      } else {
+        rotateBuff = character.rotation - Math.PI / 2;
+
+        X = positionBuff.x + Math.round(1 * Math.sin(rotateBuff));
+        Y = positionBuff.y + Math.round(1 * Math.cos(rotateBuff) * -1);
+
+        if (array[Y][X] === 4) {
+          clearInterval(timer);
+          return 0;
+        }
+      }
+    }
+  }
+
+  if (
+    positionBuff.y >= 0 &&
+    positionBuff.y < array.length &&
+    positionBuff.x >= 0 &&
+    positionBuff.x < array[0].length
+  ) {
+    // console.log("ok");
+    if (array[Y][X] === 2) {
+      positionBuff.x = X;
+      positionBuff.y = Y;
+
+      characterBuff.position = positionBuff;
+      characterBuff.rotation = rotateBuff;
+      return characterBuff;
+    } else {
+      X = positionBuff.x + Math.round(1 * Math.sin(character.rotation));
+      Y = positionBuff.y + Math.round(1 * Math.cos(character.rotation) * -1);
+
+      if (array[Y][X] === 2) {
+        positionBuff.x = X;
+        positionBuff.y = Y;
+
+        characterBuff.position = positionBuff;
+        return characterBuff;
+      } else {
+        rotateBuff = character.rotation - Math.PI / 2;
+
+        X = positionBuff.x + Math.round(1 * Math.sin(rotateBuff));
+        Y = positionBuff.y + Math.round(1 * Math.cos(rotateBuff) * -1);
+
+        if (array[Y][X] === 2) {
+          positionBuff.x = X;
+          positionBuff.y = Y;
+
+          characterBuff.rotation = rotateBuff;
+          characterBuff.position = positionBuff;
+          return characterBuff;
+        }
+      }
+    }
+  }
+
+  return null;
+}
+
+function newMaze(array, coord1, coord2) {
+  let newArray = array;
+  let xBuff = coord2.x - coord1.x;
+  let yBuff = coord2.y - coord1.y;
+
+  addX = xBuff != 0 ? xBuff / Math.abs(xBuff) : 0;
+  addY = yBuff != 0 ? yBuff / Math.abs(yBuff) : 0;
+
+  newArray[coord1.y][coord1.x] = 2;
+  newArray[coord2.y][coord2.x] = 2;
+  newArray[coord1.y + addY][coord1.x + addX] = 2;
+
+  return newArray;
+}
+
+function addStartEnd(array, height, width) {
+  let newArray = array;
+  let startEnd = Math.floor(Math.random() * width);
+
+  while (array[1][startEnd] !== 2) {
+    startEnd = Math.floor(Math.random() * width);
+  }
+  newArray[0][startEnd] = 3;
+
+  startEnd = Math.floor(Math.random() * width);
+  while (array[height - 2][startEnd] !== 2) {
+    startEnd = Math.floor(Math.random() * width);
+  }
+  newArray[height - 1][startEnd] = 4;
+
+  return newArray;
+}
+
+function findStart(array, height, width) {
+  let point = {
+    x: 0,
+    y: 0,
+  };
+
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (array[i][j] === 3) {
+        point.x = j;
+        point.y = i;
+
+        return point;
+      }
+    }
+  }
+}
+
+function findEnd(array, height, width) {
+  let point = {
+    x: 0,
+    y: 0,
+  };
+
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (array[i][j] === 4) {
+        point.x = j;
+        point.y = i;
+
+        return point;
+      }
+    }
+  }
+}
+
+function checkWay(array, character) {
+  let positionBuff = character.position;
+
+  let X = positionBuff.x + Math.round(1 * Math.sin(character.rotation));
+  let Y = positionBuff.y + Math.round(1 * Math.cos(character.rotation) * -1);
+
+  if (
+    positionBuff.y >= 0 &&
+    positionBuff.y < array.length &&
+    positionBuff.x >= 0 &&
+    positionBuff.x < array[0].length
+  ) {
+    if (array[Y][X] === 2) {
+      positionBuff.x = X;
+      positionBuff.y = Y;
+      return positionBuff;
+    } else if (array[Y][X] === 4) {
+      let masField = document.querySelectorAll(".field>div");
+
+      for (let i = 0; i < masField.length; i++) {
+        masField[i].remove();
+      }
+
+      height += 4;
+      width += 4;
+      Draw();
+
+      character.position = findStart(fieldArray, height, width);
+    }
+  }
+  return null;
 }
